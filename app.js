b91429186de5bf4b58de25b6f3d3f56a02134ab0
jianglinChen191023 <1912623646@qq.com>
2022-03-24 10:15:15 +0800

    使用gzip减少文件体积,使传输速度更快

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const compression = require('compression')
 
 // 创建 web 服务器
 const app = express()
 
+// 启用中间件, 一定要把这行代码放到 [静态资源托管之前]
+app.use(compression())
 // 托管静态资源
 app.use(express.static('./dist'))
 
